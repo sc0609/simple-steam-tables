b511007784d0573e300a38ee2c4b78153bd8ab45
Sarthak chavhan <104637836+sc0609@users.noreply.github.com>
2025-02-04 02:15:09 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ export function Footer() {
                   }
                 }}
               >
-                Sarthak
+                Sarthak Chavhan
               </Link>
             </Typography>
             
